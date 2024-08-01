ced16e8900d6c30d2f79c62fc8531d969f131295
edgalimax <edwin.avimael.galicia@gmail.com>
2024-07-31 20:39:06 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const imageUrl = 'images/foto.jpg';  // Ruta relativa a la imagen en tu proyecto
+    const profileImg = document.getElementById('profileImg');
+    
+    profileImg.src = imageUrl;
+});
